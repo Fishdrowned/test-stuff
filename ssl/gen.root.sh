c2550e43cd1bce772299ba7d961b68f01238b01f
Christopher CHEN <chenshaoming@appgame.com>
2016-11-17 17:40:27 +0800

    refactor: remove ssl folder, it is now a individual repository
    See https://github.com/Fishdrowned/ssl

--- ssl/gen.root.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
-openssl req -newkey rsa:2048 -days 7300 -x509 -nodes -keyout out/root.key.pem -out out/root.cert.pem \
-    -subj "/C=CN/ST=Guangdong/L=Guangzhou/O=Fishdrowned/CN=Fishdrowned ROOT CA"
